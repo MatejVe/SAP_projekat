6b33d062aab921012ae4bbb6abbc71c343f166b9
Matej <matejvedak@gmail.com>
2022-12-06 15:48:21 +0100

    Initialize an Rmd notebook

--- SAP_Eigen_Vektori.R
+++ SAP_Eigen_Vektori.R
@@ -1,11 +1,12 @@
 require(dplyr)
 
 podaci = read.csv("Statistika nogometaša engleske Premier lige.csv", as.is = FALSE)
-dim(podaci)
 
-names(podaci)
+youngerThan25 = podaci[podaci["Age"] <= 25,]
 
-summary(podaci)
+teamAndMinutes = youngerThan25[c("Team","Min")]
+teamAndMinutes["Min"] <- as.numeric(as.character(teamAndMinutes$Min))
 
-podaci = podaci[!podaci$MP == 0,]
+hist(teamAndMinutes$Min)
 
+data_by_team <- group_by(.teamAndMinutes, )
